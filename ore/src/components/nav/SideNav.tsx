ce649df329697bb4609f6228a762848f09cecd14
chaeeun <0_chaeeun@naver.com>
2026-03-24 20:02:11 +0900

    feat: 좌측 네비게이션 바 구현

---
+++ ore/src/components/nav/SideNav.tsx
@@ -0,0 +1,84 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import NavButton from "./NavButton";
+
+export default function SideNav() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // todo : path 수정 필요
+  const menuList = [
+    {
+      label: "DashBoard",
+      defaultIcon: "/icons/nav/dashbord_gray.svg",
+      activeIcon: "/icons/nav/dashbord_white.svg",
+      path: "/",
+    },
+    {
+      label: "Product",
+      defaultIcon: "/icons/nav/product_gray.svg",
+      activeIcon: "/icons/nav/product_white.svg",
+      path: "/products",
+    },
+    {
+      label: "Repair",
+      defaultIcon: "/icons/nav/repair_gray.svg",
+      activeIcon: "/icons/nav/repair_white.svg",
+      path: "/repair",
+    },
+    {
+      label: "AI Chat",
+      defaultIcon: "/icons/nav/ai_gray.svg",
+      activeIcon: "/icons/nav/ai_white.svg",
+      path: "/chatbot",
+    },
+    {
+      label: "My",
+      defaultIcon: "/icons/nav/my_gray.svg",
+      activeIcon: "/icons/nav/my_white.svg",
+      path: "/mypage",
+    },
+  ];
+
+  return (
+    <div
+      className="
+        flex w-[140px] flex-col items-start gap-[20px]
+      "
+    >
+      {/* 로고 */}
+      <div className="w-full">
+        <div className="flex h-[45px] items-center justify-center rounded-[10px] bg-gray-02">
+          로고
+        </div>
+      </div>
+
+      <div className="flex h-full w-full flex-col items-start">
+        {/* 메뉴 */}
+        <div className="flex w-full flex-col gap-[20px]">
+          {menuList.map((menu) => (
+            <NavButton
+              key={menu.path}
+              defaultIcon={menu.defaultIcon}
+              activeIcon={menu.activeIcon}
+              label={menu.label}
+              active={location.pathname === menu.path}
+              onClick={() => navigate(menu.path)}
+            />
+          ))}
+        </div>
+
+        {/* 로그아웃 */}
+        <div className="mt-auto w-full">
+          <NavButton
+            defaultIcon={"/icons/nav/logout.svg"}
+            label="Log Out"
+            variant="logout"
+            onClick={() => {
+              console.log("logout");
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- ore/src/pages/Home/MainPage.tsx
+++ ore/src/pages/Home/MainPage.tsx
@@ -1,7 +1,10 @@
+import SideNav from "../../components/nav/SideNav";
+
 export default function MainPage() {
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold">메인페이지</h1>
+      <SideNav />
     </div>
   );
 }
