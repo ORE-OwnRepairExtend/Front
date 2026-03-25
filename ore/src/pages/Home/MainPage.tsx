d7f674b6599845d71931876e8e0f53e0a144d818
chaeeun <0_chaeeun@naver.com>
2026-03-25 19:07:49 +0900

    chore: MainPage.tsx 코드 정리

--- ore/src/pages/Home/MainPage.tsx
+++ ore/src/pages/Home/MainPage.tsx
@@ -1,23 +1,7 @@
-import Header from "../../components/header/Header";
-import SecondLayout from "../../layout/SecondLayout";
-
 export default function MainPage() {
   return (
     <div>
-      {/* <h1 className="text-xl font-bold">메인페이지</h1> */}
-      <SecondLayout>
-        <Header
-          title="Own Repair Extend"
-          subtitle="더 오래, 더 제대로"
-          isMain
-          showSearch
-          onSearchSubmit={() => console.log("검색")}
-          showNotification
-          hasUnreadNotification
-          onNotificationClick={() => console.log("알림 클릭")}
-        />
-        <h1 className="text-xl font-bold">메인페이지</h1>
-      </SecondLayout>
+      <h1 className="text-xl font-bold">메인페이지</h1>
     </div>
   );
 }
