695ea9a766e5f5f5ca2d025c0e446ea0b9dd804f
chaeeun <0_chaeeun@naver.com>
2026-03-26 22:20:58 +0900

    chore: mainpage 코드 복원

--- ore/src/pages/Home/MainPage.tsx
+++ ore/src/pages/Home/MainPage.tsx
@@ -1,20 +1,7 @@
-// export default function MainPage() {
-//   return (
-//     <div>
-//       <h1 className="text-xl font-bold">메인페이지</h1>
-//     </div>
-//   );
-// }
-
-import Header from "../../components/header/Header";
-import MainLayout from "../../layout/MainLayout";
-
 export default function MainPage() {
   return (
     <div>
-      <MainLayout>
-        <Header title="메인페이지테스트" />
-      </MainLayout>
+      <h1 className="text-xl font-bold">메인페이지</h1>
     </div>
   );
 }
