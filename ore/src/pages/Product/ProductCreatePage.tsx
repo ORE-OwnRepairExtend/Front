33ac1beede83ac088965db839a8bf79310a1f6ec
chaeeun <0_chaeeun@naver.com>
2026-05-04 22:38:09 +0900

    feat: 제품등록 inputbox 컴포넌트화

--- ore/src/pages/Product/ProductCreatePage.tsx
+++ ore/src/pages/Product/ProductCreatePage.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import ProductInputBox from "../../components/product/ProductInputBox";
+
 export default function ProductCreatePage() {
+  const [productName, setProductName] = useState("");
+  const [purchaseDate, setPurchaseDate] = useState("");
+  const [warrantyPeriod, setWarrantyPeriod] = useState("");
+  const [replacementCycle, setReplacementCycle] = useState("");
+  const [memo, setMemo] = useState("");
+
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold">제품 등록</h1>
+      <ProductInputBox
+        value=""
+        onChange={(e) => setProductName(e.target.value)}
+        placeholder="제품명을 입력해주세요"
+        className="w-[250px]"
+      />
+      <ProductInputBox
+        variant="date"
+        value={purchaseDate}
+        onChange={(e) => setPurchaseDate(e.target.value)}
+        className="w-[120px]"
+      />
+      <ProductInputBox
+        value={warrantyPeriod}
+        onChange={(e) => setWarrantyPeriod(e.target.value)}
+        suffix="개월"
+        className="w-[100px] "
+      />
+      <ProductInputBox
+        value={warrantyPeriod}
+        onChange={(e) => setWarrantyPeriod(e.target.value)}
+        prefix="부품명"
+        className="w-[100px] "
+      />
+      <ProductInputBox
+        value={replacementCycle}
+        onChange={(e) => setReplacementCycle(e.target.value)}
+        prefix="교체주기"
+        suffix="개월"
+        className="w-[200px]"
+      />
+      <ProductInputBox
+        multiline
+        value={memo}
+        onChange={(e) => setMemo(e.target.value)}
+        placeholder="메모를 입력해주세요"
+        className="w-full"
+      />
     </div>
   );
 }
