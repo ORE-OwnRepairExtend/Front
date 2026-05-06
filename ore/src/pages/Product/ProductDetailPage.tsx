9a58274d48474a5dbe184e0a7aa7333d293743de
chaeeun <0_chaeeun@naver.com>
2026-05-06 23:41:27 +0900

    feat: 제품상세페이지-제품카드 컴포넌트 구현

--- ore/src/pages/Product/ProductDetailPage.tsx
+++ ore/src/pages/Product/ProductDetailPage.tsx
@@ -1,12 +1,48 @@
 import { useParams } from "react-router-dom";
+import ProductDetailCard from "../../components/product/ProductDetailCard";
+import { mockProductListResponse } from "../../mocks/products";
+import { mergeProductsWithWarranty } from "../../utils/productMapper";
+import { mockWarrantyListResponse } from "../../mocks/warranty";
+import SecondLayout from "../../layout/SecondLayout";
+import Header from "../../components/header/Header";
+import { useState } from "react";
+
+//테스트코드
 
 export default function ProductDetailPage() {
   const { productId } = useParams();
 
+  const productsWithStatus = mergeProductsWithWarranty(
+    mockProductListResponse,
+    mockWarrantyListResponse,
+  );
+
+  const product = productsWithStatus.find(
+    (item) => item.productId === productId,
+  );
+
+  const [isFavorite, setIsFavorite] = useState(product?.isFavorite ?? false);
+
+  if (!product) {
+    return <div>제품 정보를 찾을 수 없습니다.</div>;
+  }
+
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">제품 상세</h1>
-      <p>productId: {productId}</p>
-    </div>
+    <SecondLayout>
+      <Header title="제품 상세" />
+
+      <ProductDetailCard
+        imageSrc={product.imageUrl}
+        nickname={product.nickname}
+        productName={product.productName}
+        category={product.category}
+        purchaseDate={product.purchaseDate}
+        status={product.status}
+        isFavorite={isFavorite}
+        onFavoriteClick={() => {
+          setIsFavorite((prev) => !prev);
+        }}
+      />
+    </SecondLayout>
   );
 }
