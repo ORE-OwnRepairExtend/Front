3c642b1719169dd833976fec86d459d0451f7a83
chaeeun <0_chaeeun@naver.com>
2026-05-09 15:19:07 +0900

    feat:매뉴얼 컴포넌트

--- ore/src/pages/Product/ProductDetailPage.tsx
+++ ore/src/pages/Product/ProductDetailPage.tsx
@@ -6,6 +6,7 @@ import { mockWarrantyListResponse } from "../../mocks/warranty";
 import SecondLayout from "../../layout/SecondLayout";
 import Header from "../../components/header/Header";
 import { useState } from "react";
+import ProductManual from "../../components/product/ProductManual";
 
 //테스트코드
 
@@ -41,6 +42,14 @@ export default function ProductDetailPage() {
         isFavorite={isFavorite}
         onFavoriteClick={() => {
           setIsFavorite((prev) => !prev);
+        }}
+      />
+      <ProductManual
+        content={
+          "효과적인 학습 방법과 공부 전략으로는 능동적 학습, 자기 주도 학습, 그룹 스터디의 장점 등이 있습니다. 능동적 학습은 단순히 정보를 수동적으로 받아들이는 것이 아니라, 질문하고 토론하며 적극적으로 참여하는 학습 방식을 의미합니다. 자기 주도 학습은 자신의 학습 목표를 설정하고 계획을 세워 스스로 학습을 진행하는 방법입니다. 그룹 스터디는 동료들과의 협력을 통해 다양한 관점을 배우고 동기부여를 받을 수 있는 장점이 있습니다. 이러한 전략들을 활용하면 학습 효율성을 크게 높일 수 있습니다."
+        }
+        onPdfClick={() => {
+          console.log("PDF 원문 보기");
         }}
       />
     </SecondLayout>
